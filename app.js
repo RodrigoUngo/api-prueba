58b5c280ae957af0b11fc12d3c4853278ab340ed
RodrigoUngo <00075419@uca.edu.sv>
2022-07-03 23:14:08 -0600

    api

---
+++ app.js
@@ -0,0 +1,35 @@
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+var passport = require("passport");
+var authRouter = require("./routers/auth");
+//require("./auth/auth");
+require("./database/config");
+
+
+var userRouter = require("./routers/user");
+var wordRouter = require("./routers/word");
+
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(authRouter);
+
+//app.use(passport.authenticate("jwt", { session: false }));
+app.use("/users", userRouter);
+app.use("/words", wordRouter);
+
+
+
+
+module.exports = app;
